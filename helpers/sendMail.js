3236577e3f5cf8db89901deaca90a052c356a007
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2023-02-05 20:08:06 +0200

    HW 06 done

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,10 +1,10 @@
 const sendGridMail = require('@sendgrid/mail');
 
 // Getting env
-const path = require('path');
-require('dotenv').config({
-  path: path.join(__dirname, '../config/.env'),
-});
+// const path = require('path');
+// require('dotenv').config({
+//   path: path.join(__dirname, '../config/.env'),
+// });
 
 sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
 
